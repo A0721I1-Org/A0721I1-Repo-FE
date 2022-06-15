8066c3ded7d7560ff1dc3ab24be77ff63436a0be
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-06-15 16:42:48 +0700

    fix conflict

--- coffee-house/src/app/app.module.ts
+++ coffee-house/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
 import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +14,9 @@ import {HttpClientModule} from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     AppRoutingModule ,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule ,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- coffee-house/src/app/menu-module/service/menu.service.ts
+++ coffee-house/src/app/menu-module/service/menu.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@angular/core';
 import {OderDetail} from "../../model/oderDetail";
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Product} from "../../model/product";
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Product} from "../../model/product";
 import {TypeProduct} from "../../model/typeProduct";
 import {MenuOrderDTO} from "../../model/MenuOrderDTO";
+import {Observable} from "rxjs";
+import {Product} from "../../model/product";
+import {HttpClient} from "@angular/common/http";
 
 const API_URL = "http://localhost:8080/menu"
 
@@ -15,36 +12,45 @@ const API_URL = "http://localhost:8080/menu"
   providedIn: 'root'
 })
 export class MenuService {
+  /* Get url api */
   private API_URL = 'http://localhost:8080/api/products';
   private API_URL_ORDER_DETAIL = 'http://localhost:8080/api/order-detail';
+
+
   product: Product | undefined;
   // tslint:disable-next-line:variable-name
-  constructor(private _httpClient: HttpClient) {
-  }
+  constructor(private httpClient: HttpClient) { }
+
   // Lay danh sach product
   getAllProduct(): Observable<Product[]> {
-    return this._httpClient.get<Product[]>(this.API_URL);
+    return this.httpClient.get<Product[]>(this.API_URL);
   }
+
   // Lay product theo id
   findByIdProduct(id: number): Observable<Product>{
-    return this._httpClient.get<Product>(this.API_URL + '/' + id);
+    return this.httpClient.get<Product>(this.API_URL + '/' + id);
   }
+
   // get OrderDetail by id
   findByIdOrderDetail(id: number): Observable<OderDetail>{
-    return this._httpClient.get<OderDetail>(this.API_URL_ORDER_DETAIL + '/' + id);
+    return this.httpClient.get<OderDetail>(this.API_URL_ORDER_DETAIL + '/' + id);
   }
+
   // save OrdeDetail
   saveOrderDetail(orderDetail: OderDetail): Observable<OderDetail> {
-    return this._httpClient.post<OderDetail>(this.API_URL_ORDER_DETAIL, orderDetail);
+    return this.httpClient.post<OderDetail>(this.API_URL_ORDER_DETAIL, orderDetail);
   }
+
   // edit OrderDetail
   editOrderDetail(orderDetail: OderDetail): Observable<void> {
-    return this._httpClient.patch<void>(this.API_URL_ORDER_DETAIL + '/' + orderDetail.idOrderDetail, orderDetail);
+    return this.httpClient.patch<void>(this.API_URL_ORDER_DETAIL + '/' + orderDetail.idOrderDetail, orderDetail);
   }
+
   // delete OrderDetail
   deleteOrderDetail(id: number): Observable<OderDetail> {
-    return this._httpClient.delete(this.API_URL_ORDER_DETAIL + '/' + id);
+    return this.httpClient.delete(this.API_URL_ORDER_DETAIL + '/' + id);
   }
+
   /* Define size page and current page */
   currentPage:number = 0;
   sizePage:number = 4;
@@ -112,6 +118,4 @@ export class MenuService {
   getDataDTOForTable(idTable: number):Observable<MenuOrderDTO[]> {
     return this.httpClient.get<MenuOrderDTO[]>(`${API_URL}/table/${idTable}/${this.currentPageTable}&${this.sizePageTable}`);
   }
-
-  constructor(private httpClient: HttpClient) { }
 }
