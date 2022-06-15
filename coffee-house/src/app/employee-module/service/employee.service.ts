d1eaed403f2e6130f9a5c5f9aa0a69632594d6cb
vinhnado0231 <vinh.nd0231@gmail.com>
2022-06-15 20:19:56 +0700

    VinhTQ-list-delete-employee

--- coffee-house/src/app/employee-module/service/employee.service.ts
+++ coffee-house/src/app/employee-module/service/employee.service.ts
@@ -1,17 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Employee} from '../../model/employee';
-const URL_API = 'http://localhost:8080/api/employee';
+
+const URL_API = 'http://localhost:8080/manager/api/employee';
+
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
+
   // HauLST
   findByIdUser(idUser: number): Observable<Employee> {
     // @ts-ignore
     return this.httpClient.get(URL_API + '/detail/' + idUser);
+  }
+  //VinhTQ
+  getAllEmployee(): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(URL_API+'/list');
+  }
+  //VinhTQ
+  deleteEmployee(idUser: number): Observable<Employee> {
+    console.log(URL_API+'/delete/'+idUser)
+    return this.httpClient.delete<Employee>(URL_API+'/delete/'+idUser);
+  }
+  //VinhTQ
+  searchEmployee(username: string, name : string, phone : string): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(URL_API+'/search/'+username+'/'+name+'/'+phone);
   }
 }
