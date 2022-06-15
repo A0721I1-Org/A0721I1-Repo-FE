4ae6e54d41db667f458ba6047925ef67b1312774
vinhnado0231 <vinh.nd0231@gmail.com>
2022-06-15 20:19:56 +0700

    VinhTQ-list-delete-employee

--- coffee-house/src/app/employee-module/service/employee.service.ts
+++ coffee-house/src/app/employee-module/service/employee.service.ts
@@ -1,38 +1,34 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Employee} from '../../model/employee';
-import {Position} from '../../model/position';
+
 const URL_API = 'http://localhost:8080/manager/api/employee';
+
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
+
   // HauLST
   findByIdUser(idUser: number): Observable<Employee> {
     // @ts-ignore
     return this.httpClient.get(URL_API + '/detail/' + idUser);
   }
-
-  createEmployee(employee: Employee): Observable<void> {
-    // @ts-ignore
-    return this.httpClient.post(URL_API + '/create-employee',employee);
+  //VinhTQ
+  getAllEmployee(): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(URL_API+'/list');
   }
-
-  updateEmployee(employee: Employee): Observable<void> {
-    // @ts-ignore
-    return this.httpClient.put(URL_API + '/update-employee/' + employee.idEmployee,employee);
+  //VinhTQ
+  deleteEmployee(idUser: number): Observable<Employee> {
+    console.log(URL_API+'/delete/'+idUser)
+    return this.httpClient.delete<Employee>(URL_API+'/delete/'+idUser);
   }
-
-  getPosition():Observable<Position[]>{
-    // @ts-ignore
-    return this.httpClient.get(URL_API + '/position');
-  }
-
-  findByIdEmployee(id:number):Observable<Employee>{
-    // @ts-ignore
-    return this.httpClient.get(URL_API + '/find-id-employee/' + id);
+  //VinhTQ
+  searchEmployee(username: string, name : string, phone : string): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(URL_API+'/search/'+username+'/'+name+'/'+phone);
   }
 }
