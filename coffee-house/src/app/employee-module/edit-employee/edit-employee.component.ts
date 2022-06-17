6d8a2d73a739903d50ce6ad818149f64d85ca540
A0321I1-HuynhBaDan <huynhbadank16@gmail.com>
2022-06-17 08:51:18 +0700

    DanHB fix fe

--- coffee-house/src/app/employee-module/edit-employee/edit-employee.component.ts
+++ coffee-house/src/app/employee-module/edit-employee/edit-employee.component.ts
@@ -4,6 +4,7 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {EmployeeService} from "../service/employee.service";
 import {ActivatedRoute, ParamMap, Router} from "@angular/router";
 import {Employee} from "../../model/employee";
+import {User} from "../../model/user";
 
 @Component({
   selector: 'app-edit-employee',
@@ -14,6 +15,9 @@ export class EditEmployeeComponent implements OnInit {
   positions: Position[] = [];
   editEmployeeForm: FormGroup;
   employee: Employee;
+  // idUser: number;
+  // password:string;
+  user: User;
 
   constructor(private employeeService: EmployeeService,
               private router: Router,
@@ -22,37 +26,41 @@ export class EditEmployeeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-      this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
-        const id = Number(paramMap.get('id'));
-        this.employeeService.findByIdEmployee(id).subscribe(next => {
-          this.employee = next;
-          console.log(this.employee);
-          this.editEmployeeForm.patchValue({
-            idEmployee: this.employee.idEmployee,
-            nameEmployee: this.employee.nameEmployee,
-            addressEmployee:  this.employee.addressEmployee,
-            phoneEmployee:  this.employee.phoneEmployee,
-            genderEmployee:  this.employee.genderEmployee,
-            dateOfBirthEmployee:  this.employee.dateOfBirthEmployee,
-            salaryEmployee: this.employee.salaryEmployee,
-            position:  this.employee.position,
-            user:  this.employee.user.username,
-          })
-        });
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      const id = Number(paramMap.get('id'));
+      this.employeeService.findByIdEmployee(id).subscribe(next => {
+        this.employee = next;
+        // this.idUser = next.user.idUser;
+        // this.password = next.user.password;
+        this.user = next.user;
+        console.log(this.employee);
+        this.editEmployeeForm.patchValue({
+          idEmployee: this.employee.idEmployee,
+          nameEmployee: this.employee.nameEmployee,
+          addressEmployee:  this.employee.addressEmployee,
+          phoneEmployee:  this.employee.phoneEmployee,
+          genderEmployee:  this.employee.genderEmployee,
+          dateOfBirthEmployee:  this.employee.dateOfBirthEmployee,
+          salaryEmployee: this.employee.salaryEmployee,
+          position:  this.employee.position.namePosition,
+          user:  this.employee.user.username,
+        })
       });
+    });
 
-      this.editEmployeeForm = this.formBuilder.group({
-        nameEmployee: ['', [Validators.required, Validators.pattern(/^([a-vxyỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ]+)((\s{1}[a-vxyỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ]+){1,})$/)]],
-        addressEmployee:  ['', Validators.required],
-        phoneEmployee: ['', [Validators.required, Validators.pattern(/^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$/)]],
-        genderEmployee:  ['', Validators.required],
-        dateOfBirthEmployee:  ['', Validators.required],
-        salaryEmployee: ['', [Validators.required, Validators.min(100000)]],
-        position:  ['', Validators.required],
-        user:  ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9]+$/)]],
-      });
+    this.editEmployeeForm = this.formBuilder.group({
+      idEmployee: ['',],
+      nameEmployee: ['', [Validators.required,Validators.pattern(/^([a-vxyỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ]+)((\s{1}[a-vxyỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ]+){1,})$/)]],
+      addressEmployee:  ['', Validators.required],
+      phoneEmployee: ['', [Validators.required,Validators.pattern(/^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$/)]],
+      genderEmployee:  ['', Validators.required],
+      dateOfBirthEmployee:  ['', Validators.required],
+      salaryEmployee: ['', [Validators.required,Validators.min(100000)]],
+      position:  ['', Validators.required],
+      user:  ['', [Validators.required,Validators.pattern(/^[a-zA-Z0-9]+$/)]],
+    })
 
-      this.getAllPosition();
+    this.getAllPosition();
   }
 
   getAllPosition() {
@@ -61,34 +69,31 @@ export class EditEmployeeComponent implements OnInit {
         this.positions = next;
         console.log(this.positions);
       }
-    );
+    )
   }
 
   editSubmit() {
-    if (this.editEmployeeForm.invalid) {
+    if (this.editEmployeeForm.valid) {
       this.employee = this.editEmployeeForm.value;
-      let user = {
-        username: this.employee.user,
-        password: '123456',
-      };
-      this.employee.user = user;
+      this.user.username = this.employee.user;
+      this.employee.user = this.user;
       console.log(this.employee);
       for (let i = 0; i < this.positions.length; i++) {
         if ((this.employee.position) == (this.positions[i].idPosition)) {
           this.employee.position = this.positions[i];
         }
-        this.employeeService.updateEmployee(this.employee).subscribe(
-          () => {
-
-          },
-          () => {
-
-          },
-          () => {
-            this.router.navigateByUrl('');
-          },
-        );
       }
+      this.employeeService.updateEmployee(this.employee).subscribe(
+        () => {
+
+        },
+        () => {
+
+        },
+        () => {
+          this.router.navigateByUrl('')
+        },
+      );
     }
   }
 
