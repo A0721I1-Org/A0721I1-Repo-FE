4fce62bf864b399dc6a89759817dafb004c34519
A0421I1-LeTranSon <letranson1992@gmail.com>
2022-06-20 20:18:24 +0700

    Update firebase

--- coffee-house/src/app/product-module/edit-product/edit-product.component.ts
+++ coffee-house/src/app/product-module/edit-product/edit-product.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Component, Inject, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
 import {Product} from '../../model/product';
 import {ProductService} from '../service/product.service';
-import {ActivatedRoute, Router, ParamMap} from '@angular/router';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 import {TypeProduct} from '../../model/typeProduct';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {finalize} from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-product',
@@ -14,9 +16,10 @@ export class EditProductComponent implements OnInit {
 
   product: Product;
   typeProduct: TypeProduct[];
-  constructor(private fb: FormBuilder, private service: ProductService,
-              private router: Router, private activatedRoute: ActivatedRoute) {
-  }
+  next: any;
+  selectedImage: any;
+  errorImage: string;
+  imgVip = 'https://accounts.viblo.asia/assets/webpack/profile_default.0bca52a.png';
   editForm = this.fb.group({
     idProduct: ['', Validators.required],
     codeProduct: ['', Validators.required],
@@ -26,30 +29,71 @@ export class EditProductComponent implements OnInit {
     descriptionProduct: ['', Validators.required],
     typeProduct: ['', [Validators.required]]
   });
-  compareById( optionOne , optionTwo ): boolean {
-    return optionOne && optionTwo ? optionOne.id === optionTwo.id : optionOne === optionTwo ;
-  }
-  ngOnInit(): void {
-   this.service.findType().subscribe((data => {
-     this.typeProduct = data;
-     console.log(this.typeProduct);
-     this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
-       const id = Number(paramMap.get('id'));
-       this.service.findById(id).subscribe(next => {
-         console.log(next);
-         this.editForm.setValue(next);
-         console.log(this.editForm);
 
-       });
-     });
-   }));
+  constructor(private fb: FormBuilder, private service: ProductService,
+              private router: Router, private activatedRoute: ActivatedRoute,
+              @Inject(AngularFireStorage) private storage: AngularFireStorage) {
+  }
+
+  compareById(optionOne, optionTwo): boolean {
+    return optionOne && optionTwo ? optionOne.id === optionTwo.id : optionOne === optionTwo;
+  }
+
+  ngOnInit(): void {
+    this.service.findType().subscribe((data => {
+      this.typeProduct = data;
+      console.log(this.typeProduct);
+      this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+        const id = Number(paramMap.get('id'));
+        this.service.findById(id).subscribe(next => {
+          console.log(next);
+          this.editForm.setValue(next);
+          console.log(this.editForm);
+
+        });
+      });
+    }));
 
   }
 
   onSubmit(): void {
-    console.log(this.editForm.value);
-    this.product = this.editForm.value;
-    this.service.updateProduct(this.product).subscribe(next => this.router.navigateByUrl('/product/list'));
+    if (this.selectedImage != null) {
+      const nameImg = '/A0721I1-' + this.selectedImage.name;
+      const fileRef = this.storage.ref(nameImg);
+      this.storage.upload(nameImg, this.selectedImage).snapshotChanges().pipe(
+        finalize(() => {
+          fileRef.getDownloadURL().subscribe((url) => {
+            this.editForm.patchValue({imageProduct: url});
+            const employee = this.editForm.value;
+            this.service.updateProduct(this.editForm.value).subscribe(() => {
+              console.log(this.editForm.value);
+              this.product = this.editForm.value;
+              alert('Sua thanh cong');
+              this.service.updateProduct(this.product).subscribe(next => this.router.navigateByUrl('/product/list'));
+            }, error => {
+            });
+          });
+        })
+      ).subscribe();
+    } else {
+      const product = this.editForm.value;
+      this.service.updateProduct(this.editForm.value).subscribe(() => {
+        console.log(this.editForm.value);
+        this.product = this.editForm.value;
+        alert('Sua thanh cong');
+        this.service.updateProduct(this.product).subscribe(next => this.router.navigateByUrl('/product/list'));
+      });
+    }
+  }
+
+  showPreview(event: any) {
+    this.selectedImage = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(this.selectedImage);
+    reader.onload = e => {
+      console.log(e);
+      this.imgVip = reader.result as string;
+    };
   }
 
 }
