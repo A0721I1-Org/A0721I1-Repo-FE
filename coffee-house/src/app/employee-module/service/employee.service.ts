75a45ab6ce7264f3fd409a55dce874bf3aa95565
vinhnado0231 <vinh.nd0231@gmail.com>
2022-06-20 20:08:04 +0700

    VinhTQ-list-delete-employee

--- coffee-house/src/app/employee-module/service/employee.service.ts
+++ coffee-house/src/app/employee-module/service/employee.service.ts
@@ -2,17 +2,15 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Employee} from '../../model/employee';
+import {Position} from '../../model/position';
 
 const URL_API = 'http://localhost:8080/manager/api/employee';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-
-  /* Define size for pagination */
-  currentPage = 0;
-  sizePage = 3;
 
   constructor(private httpClient: HttpClient) {
   }
@@ -22,37 +20,49 @@ export class EmployeeService {
     // @ts-ignore
     return this.httpClient.get(URL_API + '/find-id-employee/' + idUser);
   }
-  //VinhTQ
+
+  // VinhTQ
   getAllEmployee(): Observable<Employee[]> {
-    return this.httpClient.get<Employee[]>(URL_API+'/list/page='+this.currentPage+'&size='+this.sizePage);
+    return this.httpClient.get<Employee[]>(URL_API + '/list');
   }
-  //VinhTQ
+
+  // VinhTQ
   deleteEmployee(idUser: number): Observable<Employee> {
-    return this.httpClient.delete<Employee>(URL_API+'/delete/'+idUser);
+    return this.httpClient.delete<Employee>(URL_API + '/delete/' + idUser);
   }
-  //VinhTQ
-  searchEmployee(username: string, name : string, phone : string): Observable<Employee[]> {
-    return this.httpClient.get<Employee[]>(URL_API+'/search/'+username+'/'+name+'/'+phone+'/page='+this.currentPage+'&size='+this.sizePage);
+
+  // // VinhTQ
+  searchEmployee(username: string, name: string, phone: string): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(URL_API + '/search/' + username + '/' + name + '/' + phone);
   }
 
   /* VinhTQ */
   getLengthOfEmployees(): Observable<number> {
     return this.httpClient.get<number>(URL_API + '/length/list');
   }
+
   /* VinhTQ */
-  getLengthOfEmployeeSearch(username: string, name : string, phone : string): Observable<number> {
-    return this.httpClient.get<number>(URL_API + '/length/search/'+username+'/'+name+'/'+phone);
+  getLengthOfEmployeeSearch(username: string, name: string, phone: string): Observable<number> {
+    return this.httpClient.get<number>(URL_API + '/length/search/' + username + '/' + name + '/' + phone);
   }
 
-  redirectPagination(currentPage: any) {
-    this.currentPage = currentPage;
+  createEmployee(employee: Employee): Observable<void> {
+    // @ts-ignore
+    return this.httpClient.post(URL_API + '/create-employee', employee);
   }
 
-  prevPage() {
-    this.currentPage -= 1;
+  updateEmployee(employee: Employee): Observable<void> {
+    // @ts-ignore
+    return this.httpClient.put(URL_API + '/update-employee/' + employee.idEmployee, employee);
   }
 
-  nextPage() {
-    this.currentPage += 1;
+  getPosition(): Observable<Position[]> {
+    // @ts-ignore
+    return this.httpClient.get(URL_API + '/position');
+  }
+
+  findByIdEmployee(id: number): Observable<Employee> {
+    // @ts-ignore
+    return this.httpClient.get(URL_API + '/find-id-employee/' + id);
   }
 }
