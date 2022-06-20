ca0b3a117998135032f9386fe43471f54e613ff9
vinhnado0231 <vinh.nd0231@gmail.com>
2022-06-20 15:47:53 +0700

    VinhTQ-list-delete-employee

--- coffee-house/src/app/app.module.ts
+++ coffee-house/src/app/app.module.ts
@@ -7,6 +7,7 @@ import {AppComponent} from './app.component';
 
 
 import {LoginModuleModule} from './login-module/login-module.module';
+
 // @ts-ignore
 import {ToastrModule} from 'ngx-toastr';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
