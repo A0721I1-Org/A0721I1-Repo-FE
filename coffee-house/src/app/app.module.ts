cba4d2d0f20ada73278276ff5eff0075678f8f10
Your Name <hoangnguyenviet13@gmail.com>
2022-06-20 21:09:08 +0700

    HoangNV-fix bug json

--- coffee-house/src/app/app.module.ts
+++ coffee-house/src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import {LoginModuleModule} from './login-module/login-module.module';
-// @ts-ignore
 import {ToastrModule} from 'ngx-toastr';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-// @ts-ignore
 import {JWT_OPTIONS, JwtHelperService, JwtModule} from '@auth0/angular-jwt';
-
 import { HomeComponent } from './home/home.component';
 import {HttpClientModule} from '@angular/common/http';
 
