d1b017dab4ce5c587991a43df4fce5695b3f4e20
A0321I1-HuynhBaDan <huynhbadank16@gmail.com>
2022-06-23 19:31:31 +0700

    DanHB

--- coffee-house/src/app/employee-module/edit-employee/edit-employee.component.ts
+++ coffee-house/src/app/employee-module/edit-employee/edit-employee.component.ts
@@ -56,7 +56,7 @@ export class EditEmployeeComponent implements OnInit {
       addressEmployee:  ['', Validators.required],
       phoneEmployee: ['', [Validators.required,Validators.pattern(/^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$/)]],
       genderEmployee:  ['', Validators.required],
-      dateOfBirthEmployee:  ['', Validators.required],
+      dateOfBirthEmployee:  ['', [Validators.required,checkAgeEdit]],
       salaryEmployee: ['', [Validators.required]],
       position:  ['', Validators.required],
       user:  ['', [Validators.required,Validators.minLength(6 ), Validators.pattern(/^(?!.*admin)+(?!.*root).*$/)]],
